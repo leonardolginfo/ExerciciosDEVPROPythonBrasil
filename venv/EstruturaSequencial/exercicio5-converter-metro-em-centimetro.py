d6bf7a56e203172a9919006027cdb9407a3991c0
Leonardo Gonçalves <leonardo@lginfo.com.br>
2021-07-21 20:57:17 -0300

    Add rtansformar metros em centímetros

---
+++ venv/EstruturaSequencial/exercicio5-converter-metro-em-centimetro.py
@@ -0,0 +1,8 @@
+#https://wiki.python.org.br/EstruturaSequencial
+#Faça um Programa que converta metros para centímetros
+
+metros = float(input('Digite o valor em metros: '))
+
+centimetros = metros* 100
+
+print(f'Você digitou {metros} em metros e o valor em centímetros é {centimetros} ')
