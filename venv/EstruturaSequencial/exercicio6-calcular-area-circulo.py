a9eea5343706f6c6b1c00e7491ba058c47fec38f
Leonardo Gonçalves <leonardo@lginfo.com.br>
2021-07-21 21:43:02 -0300

    Add exerc calculo area circulo - está truncando precisa refatorar

---
+++ venv/EstruturaSequencial/exercicio6-calcular-area-circulo.py
@@ -0,0 +1,9 @@
+#https://wiki.python.org.br/EstruturaSequencial
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+#Está truncando o resultado - é preciso refatorar
+
+pi = 3.14
+raio = float(input('Qual o valor do Raio? '))
+area = pi*(raio*raio)
+print(f'A área é: {area}')
+
